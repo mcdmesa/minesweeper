e4daf53124cee24fc259550ba8e0986e5d2609bd
MichelleDeMesa <mcdmesa@gmail.com>
2015-08-28 11:50:38 -0700

    placed bombed method

--- minesweeper.rb
+++ minesweeper.rb
@@ -18,13 +18,39 @@ class Game
 end
 
 class Board
-
-  def self.populate
-
-  end
+  attr_reader :pos_taken
 
   def initialize
     @grid = Board.populate
+    @pos_taken = []
+  end
+
+  def populate
+
+  end
+
+  def place_bomb
+  #randomly puts bomb into grid
+    until pos_taken.size == 10
+      pos = [rand(size), rand(size)]
+
+      unless pos_taken.include?(pos)
+        pos_taken << pos
+        self[pos] = Tile.new("B")
+      end
+    end
+  end
+
+  def adj_pos(pos)
+    x, y = pos
+    arr = []
+
+    (-1..1).to_a.each do |i|
+      (-1..1).to_a.each do |j|
+        arr << [x + i, y + j] if ((x + i).abs + (y + j).abs) <= 2
+      end
+    end
+    
   end
 
   def [](pos)
@@ -37,7 +63,7 @@ class Board
     @grid[x][y] = val
   end
 
-  def make_grid(size = 3)
+  def make_grid(size = 9)
     @grid = Array.new(size) { Array.new(size) }
   end
 
@@ -52,5 +78,13 @@ class Tile
 
   def initialize(val)
     @value = val
+  end
+
+  def reveal
+
+  end
+
+  def value
+
   end
 end
